63a10733920e4858143d8f08d41881d8e4d4a0ea
Juhnowski <juhnowski@gmail.com>
2018-05-16 05:31:45 +0300

    first commit

---
+++ src/com/juhnowski/Main.java
@@ -0,0 +1,14 @@
+package com.juhnowski;
+
+public class Main {
+    public static String hello = "oops!";
+
+    public static void main(String[] args) {
+        initHelloWorld();
+	    System.out.println(hello);
+    }
+
+    public static void initHelloWorld(){
+        String hello = "Hello World!";
+    }
+}
